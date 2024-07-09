ea59797daf3910d89672c47fa9cea936c74ed486
nguyenngocdue <duengocnguyen@gmail.com>
2024-07-09 11:06:44 +0700

    perspective and orthographicCamera

--- script.js
+++ script.js
@@ -4,16 +4,15 @@ import * as THREE from 'three'
 const scene = new THREE.Scene();
 
 // Object
-const geometry = new THREE.BoxGeometry(1, 1, 1);
+const geometry = new THREE.BoxGeometry(1, 1, 1, 5, 5, 5);
 const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
-const mesh = new THREE.Mesh(geometry, material);
+const wireframeMaterial = new THREE.MeshBasicMaterial({ color: 0xff0000, wireframe: false });
 
+const mesh = new THREE.Mesh(geometry, wireframeMaterial);
 scene.add(mesh);
-mesh.position.x = 0
-mesh.position.y = 0
-mesh.rotation.z = 0;
-
-
+//mesh.position.x = 2
+// mesh.position.y = 0
+// mesh.rotation.z = 0;
 
 // size
 const sizes = {
@@ -21,12 +20,17 @@ const sizes = {
     height: 600
 }
 
-
-// camera
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
+// Camera
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 1000);
+// const aspectRatio = sizes.width / sizes.height;
+// const camera = new THREE.OrthographicCamera(-1, 1, 1, -1, 0.1, 1000)
+// const camera = new THREE.OrthographicCamera(-1 * aspectRatio, 1 * aspectRatio, 1 * aspectRatio, -1 * aspectRatio, 0.1, 1000);
+// camera.position.z = 5;
+// camera.position.x = 2;
+// camera.position.y = 2;
 camera.position.z = 3;
+camera.lookAt(mesh.position);
 scene.add(camera);
-
 
 // canvas
 const canvas = document.querySelector('canvas.webgl');
@@ -35,12 +39,30 @@ const renderer = new THREE.WebGLRenderer({ canvas: canvas });
 renderer.setSize(sizes.width, sizes.height);
 renderer.render(scene, camera);
 
+
+// Animate
+const clock = new THREE.Clock();
+const tick = () => {
+    const elapsedTime = clock.getElapsedTime();
+    // // Update objects
+    // mesh.rotation.y = elapsedTime
+    // Render
+    renderer.render(scene, camera);
+
+    // Call tick again on the next frame
+    window.requestAnimationFrame(tick);
+}
+tick()
+
+
+
+
 // Axes Helpers
-const axesHelper = new THREE.AxesHelper(2);
-mesh.scale.x = 2
-mesh.scale.y = 0.25
-mesh.scale.z = 0.5
-scene.add(axesHelper);
+// const axesHelper = new THREE.AxesHelper(2);
+// mesh.scale.x = 0
+// mesh.scale.y = 0
+// mesh.scale.z = 0
+// scene.add(axesHelper);
 
 
 function animate() {
@@ -48,10 +70,9 @@ function animate() {
     // console.log(mesh.position.normalize())
     requestAnimationFrame(animate);
     renderer.render(scene, camera);
-
 }
 
-animate(); // Start the animation loop
+// animate(); // Start the animation loop
 // Slider controls
 const xSlider = document.getElementById('x-slider');
 const ySlider = document.getElementById('y-slider');
@@ -70,5 +91,6 @@ zSlider.addEventListener('input', function () {
 });
 
 
-mesh.rotation.x = Math.PI * 0.25
-mesh.rotation.y = Math.PI * 0.25
+// mesh.rotation.x = Math.PI * 0.25
+// mesh.rotation.y = Math.PI * 0.25
+// mesh.rotation.z = Math.PI * 0.25
